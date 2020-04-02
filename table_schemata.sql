6742d7664362a713a2b5c4374c2b3e178957c2f7
Rosario Chiovaro <rosario.chiovaro@gmail.com>
2020-04-02 14:09:35 -0400

    Update table_schemata.sql

--- table_schemata.sql
+++ table_schemata.sql
@@ -1,24 +1,10 @@
 CREATE TABLE departments (
-	dept_no VARCHAR,
+	dept_no VARCHAR PRIMARY KEY,
 	dept_name VARCHAR
 );
 
-CREATE TABLE dept_emp (
-  emp_no INT,
-  dept_no VARCHAR,
-  from_date DATE,
-  to_date DATE
-);
-
-CREATE TABLE dept_manager(
-  dept_no VARCHAR,
-  emp_no INT,
-  from_date DATE,
-  to_date DATE
-);
-
 CREATE TABLE employees(
-  emp_no INT,
+  emp_no INT PRIMARY KEY,
   birth_date DATE,
   first_name VARCHAR,
   last_name VARCHAR,
@@ -26,16 +12,36 @@ CREATE TABLE employees(
   hire_date DATE
 );
 
+CREATE TABLE dept_emp (
+  emp_no INT ,
+  dept_no VARCHAR,
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+  dept_no VARCHAR,
+  emp_no INT,
+  from_date DATE,
+  to_date DATE,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
 CREATE TABLE salaries(
   emp_no INT,
   salary INT,
   from_date DATE,
-  to_date DATE
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE titles(
   emp_no INT,
   title VARCHAR,
   from_date DATE,
-  to_date DATE
+  to_date DATE,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
